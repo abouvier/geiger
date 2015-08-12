13a224f12da6537a6d9fca2620368b7d2725fd1c
abouvier <abouvier@student.42.fr>
2015-08-12 13:33:34 +0200

    init

---
+++ geiger.ino
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   geiger.ino                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouvier <abouvier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/04 23:30:42 by abouvier          #+#    #+#             */
+/*   Updated: 2015/08/12 13:32:23 by abouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <EEPROM.h>
+#include <limits.h>
+
+const int pin = 2;
+const int interrupt = 0;
+const int speed = 9600;
+
+int size;
+int index;
+unsigned long max;
+unsigned long timer;
+unsigned long buffer[60];
+volatile unsigned long count;
+unsigned long total;
+unsigned long cpm;
+
+static void load()
+{
+	index = 0;
+	memset(buffer, 0, sizeof(buffer)); // garder les anciennes valeurs ?
+	size = constrain(size, 1, 60);
+	max = ULONG_MAX / size;
+	if (count > max)
+		count = max;
+	total = 0;
+	cpm = 0;
+}
+
+static void pulse()
+{
+	//while (digitalRead(pin) == LOW);
+	if (count < max)
+		count++;
+}
+
+void setup()
+{
+	Serial.begin(speed);
+	pinMode(pin, INPUT_PULLUP);
+	attachInterrupt(interrupt, pulse, FALLING);
+	size = EEPROM.read(0);
+	load();
+}
+
+void loop()
+{
+	unsigned long now = millis();
+
+	if (now - timer >= 1000)
+	{
+		total -= buffer[index];
+		buffer[index] = count;
+		count -= buffer[index];
+		total += buffer[index];
+		if (total < ULONG_MAX / 60)
+			cpm = (total * 60) / size;
+		else
+			cpm = max;
+		if (++index >= size)
+			index = 0;
+		timer = now;
+	}
+	Serial.println(cpm);
+	delay(1);
+}
+
+void serialEvent()
+{
+	size = Serial.parseInt();
+	Serial.readStringUntil('\n');
+	EEPROM.update(0, constrain(size, 1, 60));
+	load();
+}
