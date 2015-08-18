befa085ef3923e069d75564aba5665d2694f73ed
abouvier <abouvier@student.42.fr>
2015-08-18 05:41:31 +0200

    radex method

--- geiger.ino
+++ geiger.ino
@@ -6,7 +6,7 @@
 /*   By: abouvier <abouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/04 23:30:42 by abouvier          #+#    #+#             */
-/*   Updated: 2015/08/16 00:45:56 by abouvier         ###   ########.fr       */
+/*   Updated: 2015/08/17 06:46:47 by abouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,28 @@ const int pin = 2;
 const int interrupt = 0;
 const int speed = 9600;
 
-const int size = 10;
+const int size = 60;
 
 Timer timer;
 volatile word count;
 MovingAverage<word, size> buffer;
 MovingAverage<unsigned long, 4> history;
-unsigned long cpm;
-unsigned long cpm2;
+unsigned long cpm[2];
 
 void setup()
 {
 	Serial.begin(speed);
 	pinMode(pin, INPUT_PULLUP);
 	attachInterrupt(interrupt, pulse, FALLING);
-	timer.every(10000, tik);
-	timer.every(1000, tok);
+	//timer.every(10000, longCycle);
+	timer.every(1000, shortCycle);
 }
 
 void loop()
 {
 	timer.update();
-	Serial.println(cpm);
-	delay(1);
+	/*Serial.println(cpm[0]);
+	delay(1);*/
 }
 
 static void pulse()
@@ -51,15 +50,19 @@ static void pulse()
 		count++;
 }
 
-static void tik()
-{
-	history.push(buffer.average());
-	cpm2 = history.average();
-}
-
-static void tok()
+static void shortCycle()
 {
 	buffer.push(const_cast<word &>(count));
-	cpm = (60 * buffer.average() + 15 * cpm2) / 20;
+	cpm[0] = buffer.sum();
+	/*cpm[0] = buffer.sum() * 6;
+	if (!history.empty())
+		cpm[0] = (cpm[0] + cpm[1]) / 2;*/
 	count -= buffer.back();
+	Serial.println(cpm[0]);
+}
+
+static void longCycle()
+{
+	history.push(cpm[0]);
+	cpm[1] = history.average();
 }
